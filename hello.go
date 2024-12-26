8ddd59b1d0de076dff628cb8893ff016870afbdf
Danylo Tkach <danylo.tkach@gmail.com>
2024-12-26 16:07:34 +0200

    extended functionallity of 'Hello, World!' program

--- hello.go
+++ hello.go
@@ -2,15 +2,40 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const(
+	spanish = "Spanish"
+	french = "French"
+	ukrainian = "Ukrainian"
 
-func Hello(name string) string {
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix = "Bonjour, "
+	ukrainianHelloPrefix = "Здорові були, "
+)
+
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name + "!"
+
+	return greetingPrefix(language) + name + "!"
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	case ukrainian:
+		prefix = ukrainianHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(Hello("Dan"))
+	fmt.Println(Hello("Dan", ""))
 }
--- hello_test.go
+++ hello_test.go
@@ -4,16 +4,32 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Dan")
+		got := Hello("Dan", "")
 		want := "Hello, Dan!"
-		
 		assertCorrectingMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World!' when supplied with an empty string", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World!"
-		
+		assertCorrectingMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Diego", "Spanish")
+		want := "Hola, Diego!"
+		assertCorrectingMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Louis", "French")
+		want := "Bonjour, Louis!"
+		assertCorrectingMessage(t, got, want)
+	})
+
+	t.Run("in Ukrainian", func(t *testing.T) {
+		got := Hello("Данило", "Ukrainian")
+		want := "Здорові були, Данило!"
 		assertCorrectingMessage(t, got, want)
 	})
 }
