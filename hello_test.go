c53547f73a1b96f18cc9789b2432bb86be832137
Danylo Tkach <danylo.tkach@gmail.com>
2024-12-26 15:17:35 +0200

    basic 'Hello, World!' program with test

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Dan")
-	want := "Hello, Dan!"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Dan")
+		want := "Hello, Dan!"
+		
+		assertCorrectingMessage(t, got, want)
+	})
 
+	t.Run("say 'Hello, World!' when supplied with an empty string", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World!"
+		
+		assertCorrectingMessage(t, got, want)
+	})
+}
+
+func assertCorrectingMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		t.Errorf("got %q want %q", got,  want)
 	}
 }
